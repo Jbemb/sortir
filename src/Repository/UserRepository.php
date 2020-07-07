b781483c7ede64a636378158b2532ca4948970c7
andrea <andrearaiatea@gmail.com>
2020-07-07 12:23:49 +0200

    login email and username ok, it works

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -15,7 +16,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface, UserLoaderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
@@ -37,6 +38,22 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
     }
 
     //Create function find by email or username!!!!!
+
+    public function loadUserByUsername($usernameOrEmail): ?User
+    {
+       return $this->createQueryBuilder('u')
+            ->andWhere('u.username = :param'  )
+            ->orWhere('u.email = :param')
+            ->setParameter('param', $usernameOrEmail)
+            ->getQuery()
+           ->getOneOrNullResult();
+
+
+    }
+
+
+
+
 
     // /**
     //  * @return User[] Returns an array of User objects
