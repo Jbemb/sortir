f4950084529233df8cc854d3e901c30c94787a58
Janet <jbembe12@yahoo.com>
2020-07-07 15:37:48 +0200

    layout update of add event and controller

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,18 +2,57 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
 use App\Form\EventType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
     /**
-     * @Route("/sortie/ajouter", name="event_add")
+     * @Route("/sortie/ajouter", name="event_add", methods={"GET", "Post"})
+     *
      */
-    public function add()
+    public function add(Request $request, EntityManagerInterface $em)
     {
-        $eventForm = $this->createForm(EventType::class);
+        $user = $this->security->getUser();
+        $event = new Event();
+        $event->setOrganiser($user);
+        $event->setCampus($user->getCampus);
+        $eventForm = $this->createForm(EventType::class, $event);
+
+        $eventForm -> handleRequest($request);
+
+        if ($eventForm->isSubmitted() && $eventForm->isValid()){
+
+            if(published){
+                $event->setState();
+
+                $em->persist($event);
+                $em->flush();
+
+                $this->addFlash("success", "Sortie publiée");
+                return $this->redirectToRoute("event_post", ["id" => $event->getId()]);
+            } elseif (saved){
+                $event->setState();
+
+                $em->persist($event);
+                $em->flush();
+
+                $this->addFlash("success", "Sortie enregistréé");
+                return $this->redirectToRoute("event_post", ["id" => $event->getId()]);
+            } else{
+                $em->persist($event);
+                $em->flush();
+
+                $this->addFlash("cancel", "Pas de sortie crée");
+                return $this->redirectToRoute("home", ["id" => $event->getId()]);
+            }
+
+
+        }
 
         return $this->render('event/add.html.twig', [
             'eventForm' => $eventForm->createView()
--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -3,7 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Event;
+use App\Entity\Place;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +19,34 @@ class EventType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('startDateTime')
-            ->add('duration')
-            ->add('place')
-            ->add('maxParticipant')
-            ->add('eventInfo')
-            ->add('inscriptionLimit')
-
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la sortie'
+            ])
+            ->add('startDateTime', DateTimeType::class, [
+                'label' => 'Date et heure de la sortie'
+            ])
+            ->add('duration', NumberType::class, [
+                'label' => 'Durée',
+                'attr' => ['placeholder' => 90],
+                'attr' => ['min' => 0]
+            ])
+            ->add('place', EntityType::class, [
+                'label' => 'Lieu',
+                'class' => Place::class,
+                'choice_label' => 'name'
+            ])
+            ->add('maxParticipant', NumberType::class, [
+                'label' => 'Nombre de place'
+            ])
+            ->add('eventInfo', TextareaType::class, [
+                'label' => 'Description et infos'
+            ])
+            ->add('inscriptionLimit', DateType::class, [
+                'label'=> "Date limite d'inscription"
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Enregistrer'])
+            ->add('saveAndAdd', SubmitType::class, ['label' => 'Publier la sortie'])
+            ->add('cancel', SubmitType::class, ['label' => 'Annuler'])
         ;
     }
 
