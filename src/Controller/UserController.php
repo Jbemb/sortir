fcaa01dcae3a7b1116872804d4da3d57c6fd60c4
Leslie <leslie.bileckot@gmail.com>
2020-07-07 14:28:32 +0200

    Modify UserController.php & UserUpdateType.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,25 +8,36 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class UserController extends AbstractController
 {
+
+    private $security;
+    /**
+     * Constructeur pour récupérer le user
+     */
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     /**
      * @Route("/user-update/{id}", name="user_update")
      */
-    public function userUpdate($id, EntityManagerInterface $em, Request $request)
+    public function userUpdate(EntityManagerInterface $em, Request $request)
     {
         //récupère le UserRepository
-        $UserRepo = $this->getDoctrine()->getRepository(User::class);
+        $userRepo = $this->getDoctrine()->getRepository(User::class);
         //Requête SQL pour récupérer les infos du user
-        $user = $UserRepo->findUserByIdWithCampus($id);
+        $user = new User();
+        $user = $this->security->getUser();
         dump($user);
-        //création de mon formulaire
-        $UserUpdateForm = $this->createForm(UserUpdateType::class, $user);
+        $userUpdateForm = $this->createForm(UserUpdateType::class, $user);
         //récupère les infos inscrit dans le form
-        $UserUpdateForm->handleRequest($request);
+        $userUpdateForm->handleRequest($request);
         //vérifie la validiter du formulaire
-        if ($UserUpdateForm->isSubmitted()){
+        if ($userUpdateForm->isSubmitted()){
             $em->persist($user);
             $em->flush();
 
@@ -37,7 +48,7 @@ class UserController extends AbstractController
         return $this->render('user/update.html.twig', [
             'controller_name' => 'UserController',
             'user'=>$user,
-            'UserForm'=>$UserUpdateForm->createView(),
+            'UserForm'=>$userUpdateForm->createView(),
         ]);
     }
 
--- src/Form/UserUpdateType.php
+++ src/Form/UserUpdateType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +17,32 @@ class UserUpdateType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('roles')
-            ->add('password')
-            ->add('lastName')
-            ->add('firstName')
-            ->add('telephone')
-            ->add('email')
-            ->add('isActive')
-            ->add('campus')
+            ->add('username', TextType::class, [
+                'label'=>'Pseudo'
+            ])
+            ->add('firstName', TextType::class, [
+                'label'=>'Prénom'
+            ])
+            ->add('lastName', TextType::class, [
+                'label'=>'Nom'
+            ])
+            ->add('telephone', TextType::class, [
+                'label'=>'Télephone'
+            ])
+            ->add('email', EmailType::class, [
+                'label'=>'Email'
+            ])
+            ->add('password', RepeatedType::class, [
+                'type'=> PasswordType::class,
+                'first_options'=>['label'=>'Mot de passe'],
+                'second_options'=>['label'=>'Confirmation'],
+                'required'=>true,
+                ''
+            ])
+
+            ->add('campus', ChoiceType::class, [
+                'label'=>'Campus'
+            ])
 
         ;
     }
