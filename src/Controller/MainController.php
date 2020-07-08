f3c25014897dd448e412c7f46e10962b54689aba
Frédérick <fmotot@gmail.com>
2020-07-08 16:52:43 +0200

    Start of Search Function

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,20 +5,20 @@ namespace App\Controller;
 use App\Entity\Event;
 use App\Entity\State;
 use App\Repository\EventRepository;
-use App\Repository\StateRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Event\EventChangeState;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(EventChangeState $eventChangeState)
+    public function index(Request $request, EventRepository $eventRepository)
     {
-        $events = $eventChangeState->changeState();
-        dump($events);
+
+
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
