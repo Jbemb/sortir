ac71284510cdefc8beebfc0ea4f2f5b4cb7d127b
Leslie <leslie.bileckot@gmail.com>
2020-07-08 14:39:07 +0200

    Modify service EventChangeState.php

--- src/Event/EventChangeState.php
+++ src/Event/EventChangeState.php
@@ -26,7 +26,6 @@ class EventChangeState
         //Récupérer tous les events ouverts.
         $stateRepo = $this->doctrine->getRepository(State::class);
         $stateOpen = $stateRepo->findBy(['name' => 'Ouverte']);
-        $stateClose1 = new State();
         $stateClose = $stateRepo->findBy(['name' => 'Clôturée']);
         $eventRepo = $this->doctrine->getRepository(Event::class);
         $events = $eventRepo->findBy(['state' => $stateOpen]);
@@ -35,9 +34,14 @@ class EventChangeState
         //Passage de ouverts à clôturé
         //nbInscrits=nbMaxInscrits ou date > dateClôture
         $em = $this->doctrine->getManager();
+
         foreach ($events as $event){
-            if ($event->getInscriptionLimit() > getdate()){
-                $event->setState($stateClose);
+            if ( getdate() > $event->getInscriptionLimit()){
+                $event->setName('Essai2');
+
+
+                //$event->setState($stateClose);
+
             }
 /*            $em->persist($events);
             $em->flush();*/
