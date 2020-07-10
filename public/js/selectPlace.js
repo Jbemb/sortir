e18c8b71e8f4b9b06fde44350454bd301920dd1c
andrea <andrearaiatea@gmail.com>
2020-07-10 11:43:14 +0200

    tentative d ajax : fichier js +placeController

--- public/js/selectPlace.js
+++ public/js/selectPlace.js
@@ -1,11 +1,49 @@
 const city = document.getElementById('event_city');
 const place = document.getElementById('event_place');
 
+city.addEventListener('change', addPlaces);
+
+//prepare request
+let dbRequest = new XMLHttpRequest();
+
 function addPlaces(evt){
     //recup la valeur choisie dans la liste déroulante
-   let chosenCity = evt.target.value;
-  // let chosenCity = city.value;
-   console.log(chosenCity);
+   //let chosenCity = evt.target.value;
+   let chosenCity = city.value;
+   console.log(url + chosenCity);
+
+   dbRequest.open('POST', url);
+   // url? or file on the server with the code? ('GET', "nameoffichier.php?q="+chosenCity, true)
+
+   dbRequest.onreadystatechange = () => {
+      //when response is ready and successful
+      if(dbRequest.readyState === 4 && dbRequest.status == 200){
+         //get response
+         const response = JSON.parse(dbRequest.response);
+         console.log(response);
+
+         // add array to drop down menu
+         for(p in response) {
+            let option = document.createElement('option');
+            option.text = p.name;
+            place.add(option);
+         }
+      }
+   }
+
+   dbRequest.send( "cityId=" + chosenCity);
 }
 
-city.addEventListener('change', addPlaces);
+
+
+
+
+function addStreet(evt){
+
+   let chosenPlace = place.value;
+   console.log(chosenPlace);
+
+   dbRequest.open('GET', '/recupererLieu/' + chosenPlace);
+   // url? or file on the server with the code? ('GET', "nameoffichier.php?q="+chosenCity, true)
+   dbRequest.send();
+}
--- src/Controller/PlaceController.php
+++ src/Controller/PlaceController.php
@@ -5,18 +5,20 @@ namespace App\Controller;
 use App\Repository\CityRepository;
 use App\Repository\PlaceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PlaceController extends AbstractController
 {
     /**
-     * @Route("/placebycity", name="place_find_by_city", methods={"GET"})
+     * @Route("/placebycity", name="place_find_by_city", methods={"POST"})
      */
     public function findPlaceByCity(Request $request, CityRepository $cityRepo, PlaceRepository $placeRepo)
     {
         //get id from http request
         $cityId = $request->query->get('cityId');
+
         //find city object in bdd
         $city =$cityRepo->find($cityId);
         //get array of places based on the city
