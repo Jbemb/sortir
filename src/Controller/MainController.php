c5378e7ddbadda3ad93bcdb11b0f4424141ba54e
Frédérick <fmotot@gmail.com>
2020-07-22 16:55:54 +0200

    Revamp homepage

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -25,58 +25,26 @@ class MainController extends AbstractController
     public function index(Request $request, EventRepository $eventRepository, Security $security, SessionInterface $session)
     {
         $search = new Search();
-        // TODO add default options maybe in SearchType::class
-//        $search->setIsOrganiser(true);
-//        $search->setIsSignedUp(true);
-//        $search->setIsNotSignedUp(true);
+        // add default options maybe in SearchType::class
         $search->setCampus($this->getUser()->getCampus());
 
 
-        // TODO Get old search from session to always show previous search if user come back from elsewhere
-
-        dump($session->get('search'));
+        // Get old search from session to always show previous search if user come back from elsewhere
         if (!is_null($session->get('search'))) {
             $search->unserialize($session->get('search'));
+            // TODO find another way to manage it to get rid of deprecated function
             $search->setCampus($this->getDoctrine()->getManager()->merge($search->getCampus()));
         }
-        dump($search);
 
         $searchForm = $this->createForm(SearchType::class, $search);
 
         $searchForm->handleRequest($request);
 
-        // TODO see how to keep search object in session
-        /*$userSearch = $request->request->get('search');
-
-        unset($userSearch["submit"]);
-        unset($userSearch["_token"]);
-        dump($userSearch);
-        $session->set('userSearch', $userSearch);
-
-//        $normalizer = new ObjectNormalizer(null, null, null, new ReflectionExtractor());
-//        $serializer = new Serializer([new DateTimeNormalizer(), $normalizer]);
-//        $userSearchObject2 = $serializer->denormalize($userSearch, Search::class, '[]');
-//        dump($userSearchObject2);
-
-        //Retourne la représentation JSON d'une valeur https://github.com/pmill/doctrine-array-hydrator
-//        $userSearchJson = json_encode($userSearch);
-//        dump($userSearchJson);
-
-//        $userSearch1 = new Search();
-//        $userSearch1->setKeywords($userSearch["keywords"]);
-//        $userSearch1->setStartDate($userSearch["startDate"]);
-//        $userSearch1->setEndDate($userSearch["endDate"]);
-//        dump($userSearch1);
-//
-//        $userSearchObject = $this->get('serializer')->deserialize($userSearchJson, Search::class, 'json');
-//        dump($userSearchObject);
-*/
         $events = [];
 
         if ($searchForm->isSubmitted()) {
             $session->set('search', $search->serialize());
         }
-
 
         $events = $eventRepository->search($search, $security->getUser());
 
