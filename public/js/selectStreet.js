81edc5898ee68b67bab0b718e51dfd48b5ef5444
Leslie <leslie.bileckot@gmail.com>
2020-07-23 15:44:17 +0200

    modify selectStreet.js remove console.log

--- public/js/selectStreet.js
+++ public/js/selectStreet.js
@@ -6,7 +6,6 @@ place.addEventListener("change", addStreet);
 function addStreet(evt) {
     //récupérer la place choisie
     let chosenPlace = evt.target.value;
-    console.log(chosenPlace);
 
     //requête ajax post (url, )
     $.post(
